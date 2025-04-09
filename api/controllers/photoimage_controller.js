d94dda9f3ce29fc8b732fc8e55d8c161ff6bd5fe
Kragel <118104043+NotaDough@users.noreply.github.com>
2025-04-09 21:08:13 +0800

    Update photoimage_controller.js

--- api/controllers/photoimage_controller.js
+++ api/controllers/photoimage_controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('./api/connections/pool');
+const pool = require('../connections/pool');
 const fs = require('fs').promises;
 const path = require('path');
 const { 
